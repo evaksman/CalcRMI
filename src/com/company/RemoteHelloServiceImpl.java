857ba82de2d91054685606809ea444b358728ddd
Чернышева Екатерина <33285655+echernysheva@users.noreply.github.com>
2019-03-24 17:58:47 +0500

    Вычисление выражения
    без вызова удаленных методов-операций

--- src/com/company/RemoteHelloServiceImpl.java
+++ src/com/company/RemoteHelloServiceImpl.java
@@ -3,12 +3,14 @@ package com.company;
 import java.rmi.*;
 import java.rmi.server.*;
 import java.rmi.registry.*;
+import java.util.ArrayList;
+import java.util.Stack;
 
 public class RemoteHelloServiceImpl implements RemoteHelloService {
 
     public static final String BINDING_NAME = "sample/HelloService";
 
-    public Object sayHello(String name) {
+    public String sayHello(String name) {
         String string = "Hello, " + name + "! It is " + System.currentTimeMillis() + " ms now";
         try {
             System.out.println(name + " from " + UnicastRemoteObject.getClientHost());
@@ -20,6 +22,57 @@ public class RemoteHelloServiceImpl implements RemoteHelloService {
             System.exit(1);
         }
         return string;
+    }
+
+    private static int sum(int a, int b) { return a+b; }
+
+    private static int sub(int a, int b) { return b-a; }
+
+    private static int mult(int a, int b) { return a*b; }
+
+    private static int div(int a, int b) { return b/a; }
+
+    private static int calc(String str)
+    {
+        Stack <Integer> stack = new Stack<>(); // стек с операндами, тут же и получим значение выражения
+        ArrayList<Object> expression = new ArrayList<>(); // список для хранения выражения в обратной польской записи
+        for (Object op : str.split(",")){
+            expression.add(op);
+        }
+        for (Object s : expression){
+            switch(s.toString()) {
+                case "+":
+                    // вызов удаленного метода
+                    stack.push(sum(stack.pop(), stack.pop()));
+                    break;
+                case "-":
+                    // вызов удаленного метода
+                    stack.push(sub(stack.pop(), stack.pop()));
+                    break;
+                case "*":
+                    // вызов удаленного метода
+                    stack.push(mult(stack.pop(), stack.pop()));
+                    break;
+                case "/":
+                    // вызов удаленного метода
+                    stack.push(div(stack.pop(), stack.pop()));
+                    break;
+                default:
+                    stack.push(Integer.valueOf(s.toString()));
+                    break;
+            }
+         }
+        return stack.pop();
+    }
+
+    public int Calculation(String exp) {
+        int res = 0;
+        String str = "";
+        //String str = "6,10,+,4,-,1,1,2,*,+,/,1,+";
+        //String str2 = "8,2,5,*,+,1,3,2,*,+,4,-,/";
+        //str = ОбратнаяПолЗапись(exp);
+        res = calc(str);
+        return res;
     }
 
     public static void main(String... args) throws Exception {
