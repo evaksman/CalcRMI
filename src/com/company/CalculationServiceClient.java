87f5f65f617d035f2150a58d261ffac7572629fc
Julia Gordeeva <gordeeva96@gmail.com>
2019-03-27 04:24:11 +0500

    код ревью клиента

--- src/com/company/CalculationServiceClient.java
+++ src/com/company/CalculationServiceClient.java
@@ -1,11 +1,15 @@
 package com.company;
 
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
 import java.rmi.registry.*;
 import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class CalculationServiceClient {
+    private static RemoteCalculationService service;
+
     private static boolean checkBrackets(String exp){
         int counter = 0;
         boolean check = true;
@@ -22,22 +26,23 @@ public class CalculationServiceClient {
         return check;
     }
 
-    private static boolean checkExpression(String exp){
-        String regex = "([^0-9+\\-*/()])|([+\\-*/]{2,})|([+\\-*/]\\))";
+    private static boolean checkExpression(String exp) {
+        String regex = "([^0-9+*-/()])|([+*-/]{2,})|([+*-/]\\))";
         Pattern pattern = Pattern.compile(regex);
         Matcher matcher = pattern.matcher("(" + exp + ")");
-        if (exp.isEmpty() || matcher.find())
-            return false;
-        else
-            return checkBrackets(exp);
+        return !(exp.isEmpty() || matcher.find()) && checkBrackets(exp);
     }
 
-    public static void main(String... args) throws Exception {
+    private static void registryService() throws RemoteException, NotBoundException {
         System.out.print("Starting registry client...");
-        Registry registry = LocateRegistry.getRegistry("localhost", 2099);
-        RemoteCalculationService service = (RemoteCalculationService) registry.lookup("sample/CalcService");
-        System.out.println("OK");
 
+        Registry registry = LocateRegistry.getRegistry("localhost", 2099);
+        service = (RemoteCalculationService) registry.lookup("sample/CalcService");
+
+        System.out.println("OK");
+    }
+
+    private static void scanExpression() throws RemoteException {
         Scanner scanner = new Scanner(System.in);
         while (true)
         {
@@ -47,6 +52,16 @@ public class CalculationServiceClient {
                 System.out.println("Result: " + service.Calculation(exp));
             else
                 System.out.println("Error!");
+        }
+    }
+
+    public static void main(String... args) throws Exception {
+        try {
+            registryService();
+            scanExpression();
+        }
+        catch(Exception e) {
+            e.printStackTrace();
         }
     }
 }
