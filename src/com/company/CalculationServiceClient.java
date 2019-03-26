e2cce791e1d386c2e97944fb8d4670b3ad053590
Julia Gordeeva <gordeeva96@gmail.com>
2019-03-27 00:37:46 +0500

    проверка корректности введенного выражения на клиенте

--- src/com/company/CalculationServiceClient.java
+++ src/com/company/CalculationServiceClient.java
@@ -2,11 +2,39 @@ package com.company;
 
 import java.rmi.registry.*;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class CalculationServiceClient {
+    private static boolean checkBrackets(String exp){
+        int counter = 0;
+        boolean check = true;
+
+        for (int i = 0; i < exp.length() && check; i++) {
+            if (exp.charAt(i) == '(') counter++;
+            else if (exp.charAt(i) == ')') {
+                counter--;
+                if (counter < 0) check = false;
+            }
+        }
+        if (counter != 0) check = false;
+
+        return check;
+    }
+
+    private static boolean checkExpression(String exp){
+        String regex = "([^0-9+\\-*/()])|([+\\-*/]{2,})|([+\\-*/]\\))";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher("(" + exp + ")");
+        if (exp.isEmpty() || matcher.find())
+            return false;
+        else
+            return checkBrackets(exp);
+    }
+
     public static void main(String... args) throws Exception {
         System.out.print("Starting registry client...");
-        Registry registry = LocateRegistry.getRegistry("localhost", 2100);
+        Registry registry = LocateRegistry.getRegistry("localhost", 2099);
         RemoteCalculationService service = (RemoteCalculationService) registry.lookup("sample/CalcService");
         System.out.println("OK");
 
@@ -15,7 +43,10 @@ public class CalculationServiceClient {
         {
             System.out.println("Enter expression: ");
             String exp = scanner.nextLine();
-            System.out.println("Result: " + service.Calculation(exp));
+            if (checkExpression(exp))
+                System.out.println("Result: " + service.Calculation(exp));
+            else
+                System.out.println("Error!");
         }
     }
 }
